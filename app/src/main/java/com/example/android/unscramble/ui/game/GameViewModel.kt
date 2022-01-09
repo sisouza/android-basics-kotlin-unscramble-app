6c2c4d9fcf2bd20db96307ca204f9b781977cb7b
Sidia <sidia.reis1@gmail.com>
2022-01-09 15:55:47 -0800

    moves data variables from GameFragmento to GameViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,5 +3,14 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 //extending ViewModel Class
-class GameViewModel: ViewModel() {
+class GameViewModel : ViewModel() {
+
+    //data variables
+    private var score = 0
+    private var currentWordCount = 0
+    private var _currentScrambledWord = "test"
+
+    //sets a read-only property of _currentScrambleWord to be used for UI - immutability principle
+    val currentScrambleWord: String
+        get() = _currentScrambledWord
 }
