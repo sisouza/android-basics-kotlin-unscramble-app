aaca5f5f208a97b1a3c767309c1ecae09d809e7a
Sidia <sidia.reis1@gmail.com>
2022-01-09 16:23:15 -0800

    add log to understand ViewModel and Fragment lifecycle

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 //extending ViewModel Class
 class GameViewModel : ViewModel() {
-
+    //This block of code is run when the object instance is first created and initialized.
+    init {
+        Log.d("GameFragment", "GameViewModel created")
+    }
     //data variables
     private var score = 0
     private var currentWordCount = 0
@@ -13,4 +17,12 @@ class GameViewModel : ViewModel() {
     //sets a read-only property of _currentScrambleWord to be used for UI - immutability principle
     val currentScrambleWord: String
         get() = _currentScrambledWord
+
+    //before the ViewModel is destroyed, the onCleared() callback is called.
+    override fun onCleared() {
+        super.onCleared()
+        //track viewModel lifecycle
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
 }
